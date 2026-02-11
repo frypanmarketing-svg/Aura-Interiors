84f8db41134202d8ea9ce397f0c1ec2cd8babdc0
Yasir-Gaming <frypanmarketing@gmail.com>
2026-02-11 23:25:44 +0500

    On path to finilize header and hero section and setup interactivity

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,19 +1,35 @@
-const Header = () => (
-  <>
-    <header className="box-border w-full px-12 py-2 flex flex-row h-16 justify-between items-center bg-white/30 font-sans font-semibold backdrop-blur-[1px] absolute z-10">
-      <span>Aura</span>
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
-      <nav>
-        <ul className="flex items-center justify-center gap-x-4">
-          <li>Home</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  </>
-);
+export default function Header() {
+  return (
+    <>
+      <header className="box-border w-full px-8 md:px-12 py-2 flex flex-row h-16 justify-between items-center bg-white/30 font-sans font-semibold backdrop-blur-[1px] absolute z-10">
+        <Image
+          alt="Aura"
+          src={logo.src}
+          height={logo.height}
+          width={logo.width}
+          className="w-16 h-auto"
+        />
 
-export default Header;
+        <nav>
+          <ul className="hidden md:flex items-center justify-center gap-x-4">
+            <li>Home</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        {/*hamburger*/}
+        <img
+          src="/hamburger.svg"
+          className="w-10 h-10 block md:hidden cursor-pointer"
+          id="header-menu"
+        ></img>
+      </header>
+    </>
+  );
+}
